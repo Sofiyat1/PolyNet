803ab349dc56d0c7c8c95e10f759d676827e70e8
Sofiyat1 <sofiyatgbadamosi@gmail.com>
2026-04-06 18:43:49 +0100

    Fix: notification typo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Network from "./pages/Network";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import Notification from "./pages/Notificatoin";
+import Login from "./pages/Login";
 
 import AppLayout from "./layouts/AppLayout";
 import "./App.css";
@@ -15,6 +16,7 @@ function App() {
     <Routes>
       {/* Onboarding / Landing */}
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
 
       {/* App pages */}
       <Route element={<AppLayout />}>

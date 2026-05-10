bbf48ec183e06b01b211734b4b8471611642d61d
Sofiyat1 <sofiyatgbadamosi@gmail.com>
2026-05-10 10:13:52 +0100

    Update: ConnectionPage

--- src/pages/ConnectionPage.jsx
+++ src/pages/ConnectionPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FiX, FiSearch, FiShield, FiCheckCircle } from "react-icons/fi";
+import { FiShield, FiCheckCircle } from "react-icons/fi";
 import "./ConnectionPage.css";
 import { FaShieldAlt, FaTimesCircle, FaUserCheck } from "react-icons/fa";
 
@@ -49,12 +49,8 @@ function ConnectionPage() {
   return (
     <div className="connection-page">
       {/* HEADER */}
-      <div className="connection-header">
-        <h2>Connections</h2>
-        {/*<FiSearch
-          className="search-icon"
-          onClick={() => console.log("open search")}
-        />*/}
+        <div className="connection-header">
+          <h2>Connections</h2>
       </div>
 
       {/* REQUESTS */}
@@ -78,7 +74,10 @@ function ConnectionPage() {
                 onClick={() => handleAccept(req, "decoy")}
               >
                 <FaShieldAlt className="btn-icon" />
-                Decoy
+                <div className="btn-text">
+                  <span>Decoy</span>
+                  <small>Sees only your decoy posts</small>
+                </div>
               </button>
 
               <button
@@ -86,7 +85,10 @@ function ConnectionPage() {
                 onClick={() => handleAccept(req, "standard")}
               >
                 <FaUserCheck className="btn-icon" />
-                Standard
+                <div className="btn-text">
+                  <span>Standard</span>
+                  <small>Sees only your standard posts</small>
+                </div>
               </button>
 
               <button className="reject" onClick={() => handleReject(req.id)}>
@@ -97,34 +99,37 @@ function ConnectionPage() {
           </div>
         ))}
       </section>
-
       {/* CONNECTIONS */}
       <section className="section">
         <h3>Your Network ({networkCount})</h3>
 
         {connections.map((conn) => (
-          <div key={conn.id} className="connection-row">
-            <div className="row">
-              <div className="avatar" />
+            <div key={conn.id} className="connection-row">
+              <div className="row">
+                <div className="avatar" />
+                <div className="text-block">
+                  <div className="name-row">
+                    <p className="name">{conn.name}</p>
 
-              <div className="text-block">
-                <div className="name-wrapper">
-                  <p className="name">{conn.name}</p>
+                    <span className={`badge ${conn.access}`}>
+                      {conn.access === "decoy" ? "Decoy" : "Standard"}
+                    </span>
+                  </div>
 
-                  <span className={`badge-sup ${conn.access}`}>
-                    {conn.access === "decoy" ? "DE" : "ST"}
+                  <p className="access-info">
+                    {conn.access === "decoy"
+                      ? "Sees only your decoy posts"
+                      : "Sees only your standard posts"}
+                  </p>
+
+                  <span className="change-link" onClick={() => openModal(conn)}>
+                    Change access
                   </span>
                 </div>
-
-                <span className="change-link" onClick={() => openModal(conn)}>
-                  Change access
-                </span>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </section>
-
       {/* MODAL */}
       {activeUser && (
         <div className="modal-overlay" onClick={() => setActiveUser(null)}>
@@ -143,14 +148,20 @@ function ConnectionPage() {
                 className={selectedAccess === "decoy" ? "active" : ""}
                 onClick={() => setSelectedAccess("decoy")}
               >
-                Decoy Access
+                <div className="btn-text">
+                  <span>Decoy Access</span>
+                  <small>They will only see your decoy posts</small>
+                </div>
               </button>
 
               <button
                 className={selectedAccess === "standard" ? "active" : ""}
                 onClick={() => setSelectedAccess("standard")}
               >
-                Standard Access
+                <div className="btn-text">
+                  <span>Standard Access</span>
+                  <small>They will only see your standard posts</small>
+                </div>
               </button>
             </div>
 
